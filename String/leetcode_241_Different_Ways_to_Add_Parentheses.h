15388a992aacc547505a0ffc357dc4121e75c2c1
Dennis <ordinarydennis@gmail.com>
2023-01-02 22:31:58 +0900

    Edit LeetCode Solution - 241. Different Ways to Add Parentheses(solved again)

--- String/leetcode_241_Different_Ways_to_Add_Parentheses.h
+++ String/leetcode_241_Different_Ways_to_Add_Parentheses.h
@@ -3,25 +3,27 @@ public:
     vector<int> diffWaysToCompute(string input) {
 
         vector<int> result;
-        
-        int size = input.size();
-        
-        for (int i = 0; i < size; i++) {
 
-            char cur = input[i];
+        if (1 == input.size())
+        {
+            result.push_back(std::atoi(input.c_str()));
+            return result;
+        }
 
-            if (cur == '+' || cur == '-' || cur == '*')
+        for (int i = 0; i < input.size(); i++)
+        {
+            if ('+' == input[i] || '-' == input[i] || '*' == input[i])
             {
-                // Split input string into two parts and solve them recursively
                 vector<int> result1 = diffWaysToCompute(input.substr(0, i));
                 vector<int> result2 = diffWaysToCompute(input.substr(i + 1));
-                for (auto n1 : result1)
+
+                for (int n1 : result1)
                 {
-                    for (auto n2 : result2)
+                    for (int n2 : result2)
                     {
-                        if (cur == '+')
+                        if ('+' == input[i])
                             result.push_back(n1 + n2);
-                        else if (cur == '-')
+                        else if('-' == input[i])
                             result.push_back(n1 - n2);
                         else
                             result.push_back(n1 * n2);
@@ -29,12 +31,12 @@ public:
                 }
             }
         }
-        // if the input string contains only number
+
         if (result.empty())
-            result.push_back(atoi(input.c_str()));
+        {
+            result.push_back(std::atoi(input.c_str()));
+        }
 
         return result;
     }
 };
-
-2 - 1 - 1
