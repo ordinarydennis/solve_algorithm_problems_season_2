c265ab74ed1372e6c988348e92496e2ed6783cbc
Dennis <ordinarydennis@gmail.com>
2023-01-09 23:35:18 +0900

    Edit LeetCode Solution - 388. Longest Absolute File Path(read solution) https://leetcode.com/problems/longest-absolute-file-path/solutions/86729/very-concise-c-solution-0ms/?orderBy=most_votes

--- String/leetcode_388_Longest_Absolute_File_Path.h
+++ String/leetcode_388_Longest_Absolute_File_Path.h
@@ -1,46 +1,58 @@
 class Solution {
 public:
-	int lengthLongestPath(string input) {
+    int lengthLongestPath(string input) {
 
-		std::map<int, std::vector<string>> vv;
+        string s;
 
-		int tCount = 0;
-		int maxtCount = 0;
-	
-		string s;
+        int tCount = 0;
 
-		int dir;
-		int ret;
+        std::unordered_map<int, int> m;
 
-		for (int i = 0; i < input.size(); i++)
-		{
-			if ('\n' == input[i])
-			{
-				vv[tCount].push_back(s);
+        int isDir = 1;
 
-				if (maxtCount < tCount)
-				{
-					dir += (*vv[tCount - 1].rbegin()).size() + s.size();
-					maxtCount = tCount;
-				}
+        int ret = 0;
 
-				ret = max(ret, dir);
+        for (int i = 0; i <= input.size(); i++)
+        {
+            if ('\n' == input[i] || '\0' == input[i])
+            {
+                int size = 0;
 
-				tCount = 0;
-			}
-			else if ('\t' == input[i])
-			{
-				tCount++;
-			}
-			else
-			{
-				s = input[i];
-			}
-		}
+                if (0 < tCount)
+                {
+                    size = m[tCount - 1] + s.size() + isDir;
+                }
+                else
+                {
+                    size = s.size() + isDir;
+                }
 
-		return ret;
+                m[tCount] = size;
 
-	}
+                if (0 == isDir)
+                {
+                    ret = max(ret, size);
+                }
+
+                s.clear();
+                tCount = 0;
+                isDir = 1;
+            }
+            else if ('\t' == input[i])
+            {
+                tCount++;
+            }
+            else if ('.' == input[i])
+            {
+                isDir = 0;
+                s += input[i];
+            }
+            else
+            {
+                s += input[i];
+            }
+        }
+
+        return ret;
+    }
 };
-
-
