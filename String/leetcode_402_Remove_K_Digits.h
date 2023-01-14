2f98e97ae40fd27ce5fe438f08254a1fe6ee6ae6
Dennis <ordinarydennis@gmail.com>
2023-01-14 22:28:10 +0900

    Edit LeetCode Solution - 402. Remove K Digits(read solution) https://leetcode.com/problems/remove-k-digits/solutions/607570/c-solution-8ms-with-detailed-explanation/?orderBy=most_votes

--- String/leetcode_402_Remove_K_Digits.h
+++ String/leetcode_402_Remove_K_Digits.h
@@ -1,31 +1,26 @@
 class Solution {
 
-	int mMin = INT_MAX;
-	int mK = 0;
-
-	void removeStr(string s, int k)
-	{
-		if (mK == k)
-		{
-			mMin = std::min(mMin, std::atoi(s.c_str()));
-			return;
-		}
-
-		for (int i = 0; i < s.size() - mK - k; i++)
-		{
-			removeStr(s.substr(i, 1), k + 1);
-		}
-	}
-
 public:
-	string removeKdigits(string num, int k) {
+    string removeKdigits(string num, int k) {
 
-		mK = k;
+        string ans = "";
 
-		num.size() - k;
+        for (char& c : num)
+        {
+            while (ans.size() && ans.back() > c && k)
+            {
+                ans.pop_back();
+                k--;
+            }
 
-		removeStr(num, 0);
+            if (ans.size() || c != '0')
+                ans.push_back(c);
+        }
+        while (ans.size() && k--)
+        {
+            ans.pop_back();
+        }
+        return (ans == "") ? "0" : ans;
+    }
 
-		return std::to_string(mMin);
-	}
 };
