1542716081d9bae6a87f424def20995f2832d339
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-01-19 14:32:42 +0900

    Add LeetCode Solution - 424. Longest Repeating Character Replacement(read solution) https://leetcode.com/problems/longest-repeating-character-replacement/solutions/2805777/longest-repeating-character-replacement/?orderBy=most_votes

--- String/leetcode_424_Longest_Repeating_Character_Replacement.h
+++ String/leetcode_424_Longest_Repeating_Character_Replacement.h
@@ -1,65 +1,26 @@
 class Solution {
-
 public:
 	int characterReplacement(string s, int k) {
 
-		int max = INT_MIN;
-
-		for (int i = 0; i < s.size() - 1; i++)
+		int size = s.size(); int ret = 0;
+		
+		vector<int> count(26, 0);
+		
+		int start = 0; int end = 0; int globalMaxFreq = 0;
+		
+		for (; end < size; end++)
 		{
-			char c = s[i];
-			int count = 1;
-			int kk = k;
+			count[s[end] - 'A'] += 1;
 
-			for (int a = i + 1; a < s.size(); a++)
+			globalMaxFreq = max(globalMaxFreq, count[s[end] - 'A']);
+			
+			if ((end - start + 1) - globalMaxFreq > k)
 			{
-				if (c == s[a])
-				{
-					count++;
-				}
-				else
-				{
-					if (kk)
-					{
-						kk--;
-						count++;
-					}
-					else
-					{
-						break;
-					}
-				}
+				ret = max(ret, (end - start));
+				count[s[start] - 'A'] -= 1;
+				start += 1;
 			}
-
-			if (0 == kk || 0 == i)
-			{
-				max = std::max(max, count);
-				continue;
-			}
-
-			for (int a = i - 1; 0 < a; a--)
-			{
-				if (c == s[a])
-				{
-					count++;
-				}
-				else
-				{
-					if (kk)
-					{
-						kk--;
-						count++;
-					}
-					else
-					{
-						break;
-					}
-				}
-			}
-			max = std::max(max, count);
-
 		}
-		return max;
+		return max(ret, end - start);
 	}
 };
-
