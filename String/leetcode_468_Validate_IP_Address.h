6e6774bc67ba9c8ea843588d05286e5fef379679
ordinarydennis <ordinarydennis@gmail.com>
2023-01-29 17:12:17 +0900

    Edit LeetCode Solution  - 468. Validate IP Address(solved again) https://leetcode.com/problems/validate-ip-address/solutions/238511/c-split/?orderBy=most_votes

--- String/leetcode_468_Validate_IP_Address.h
+++ String/leetcode_468_Validate_IP_Address.h
@@ -1,63 +1,119 @@
 class Solution {
+
+	int IsIPv4(const string& str)
+	{
+		return count(str.begin(), str.end(), '.');
+	}
+
+	string CheckValidIPv4(const string& ip)
+	{
+		if (count(ip.begin(), ip.end(), '.') > 3)
+		{
+			return "Neither";
+		}
+
+		auto strList = split(ip, '.');
+
+		if (4 != strList.size())
+		{
+			return "Neither";
+		}
+
+		for (int i = 0; i < strList.size(); i++)
+		{
+			const auto& str = strList[i];
+
+			if (str.size() > 3 || str.empty())
+			{
+				return "Neither";
+			}
+
+			if (1 < str.size() && '0' == str[0])
+			{
+				return "Neither";
+			}
+
+			for (char c : str)
+			{
+				if (false == isdigit(c))
+				{
+					return "Neither";
+				}
+			}
+
+			if (255 < std::stoi(str))
+			{
+				return "Neither";
+			}
+		}
+
+		return "IPv4";
+	}
+
+	string CheckValidIPv6(const string& ip)
+	{
+		auto strList = split(ip, ':');
+
+		if (count(ip.begin(), ip.end(), ':') > 7)
+		{
+			return "Neither";
+		}
+
+		if (strList.size() != 8)
+		{
+			return "Neither";
+		}
+
+		for (int i = 0; i < strList.size(); i++)
+		{
+			const string str = strList[i];
+
+			if (str.size() > 4 || str.empty())
+			{
+				return "Neither";
+			}
+
+			for (char c : str)
+			{
+				if (isdigit(c) && isalpha(c))
+				{
+					return "Neither";
+				}
+
+				if (isalpha(c) && toupper(c) > 'F')
+				{
+					return "Neither";
+				}
+			}
+		}
+
+		return "IPv6";
+	}
+
+	vector<string> split(const string& str, char deli)
+	{
+		vector<string> ret;
+
+		istringstream in(str);
+
+		string s;
+
+		while (getline(in, s, deli))
+		{
+			ret.push_back(s);
+		}
+
+		return ret;
+	}
+
+
 public:
-    string validIPAddress(string IP) {
+	string validIPAddress(string IP) {
 
-        return validIPv4(IP) ? "IPv4" : (validIPv6(IP) ? "IPv6" : "Neither");
+		return IsIPv4(IP) ? CheckValidIPv4(IP) : CheckValidIPv6(IP);
 
-    }
-private:
-    bool validIPv4(string IP) {
-        if (count(IP.begin(), IP.end(), '.') != 3) {
-            return false;
-        }
-        vector<string> parts = split(IP, '.');
-        if (parts.size() != 4) {
-            return false;
-        }
-        for (string part : parts) {
-            if (part.empty() || part.size() > 3 || part.size() > 1 && part[0] == '0') {
-                return false;
-            }
-            for (char c : part) {
-                if (!isdigit(c)) {
-                    return false;
-                }
-            }
-            if (stoi(part) > 255) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool validIPv6(string IP) {
-        if (count(IP.begin(), IP.end(), ':') != 7) {
-            return false;
-        }
-        vector<string> parts = split(IP, ':');
-        if (parts.size() != 8) {
-            return false;
-        }
-        for (string part : parts) {
-            if (part.empty() || part.size() > 4) {
-                return false;
-            }
-            for (char c : part) {
-                if (!isdigit(c) && (!isalpha(c) || toupper(c) > 'F')) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    vector<string> split(string s, char c) {
-        vector<string> parts;
-        string part;
-        istringstream in(s);
-        while (getline(in, part, c)) {
-            parts.push_back(part);
-        }
-        return parts;
-    }
+	}
 };
+
+//refer to link blow
+//https://leetcode.com/problems/validate-ip-address/solutions/238511/c-split/
