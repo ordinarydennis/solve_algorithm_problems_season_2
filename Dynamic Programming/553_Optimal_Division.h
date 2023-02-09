17abed44e0f3e149c14b4c44dd853dfb2dcd0b77
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-02-09 22:06:17 +0900

    Add LeetCode Solution - 553. Optimal Division(fail)

---
+++ Dynamic Programming/553_Optimal_Division.h
@@ -0,0 +1,45 @@
+class Solution {
+
+	vector<bool> checks;
+
+	string max = 0.f;
+
+	void calc(vector<int>& nums, int pos, float f)
+	{
+		if (nums.size() < pos)
+		{
+			max = std::max(max, f);
+			return;
+		}
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (false == checks[i])
+			{
+				checks[i] = true;
+
+				if (0 == f)
+				{
+					calc(nums, pos + 1, nums[i]);
+				}
+				else
+				{
+					calc(nums, pos + 1, f / nums[i]);
+				}
+
+				checks[i] = false;
+			}
+		}
+
+	}
+
+public:
+	string optimalDivision(vector<int>& nums) {
+
+		checks.resize(nums.size());
+
+		calc(nums, 0, 0.f);
+
+		return max;
+	}
+};
