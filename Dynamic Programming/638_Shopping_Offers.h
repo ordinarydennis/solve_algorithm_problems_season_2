2908e27986d88191d0912034f47bfc42e67fadf0
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-02-13 21:47:55 +0900

    Add LeetCode Solution - 638. Shopping Offers(fail)

---
+++ Dynamic Programming/638_Shopping_Offers.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+
+	}
+};
+
+
+
+Input: price = [2, 5], special = [[3, 0, 5], [1, 2, 10]], needs = [3, 2]
+
+Output : 14
+Explanation : There are two kinds of items, Aand B.Their prices are $2and $5 respectively.
+In special offer 1, you can pay $5 for 3A and 0B
+In special offer 2, you can pay $10 for 1A and 2B.
+You need to buy 3A and 2B, so you may pay $10 for 1A and 2B(special offer #2), and $4 for 2A.
