6f994e5e461ebe06dd131b45103d0f7630943604
ordinarydennis <ordinarydennis@gmail.com>
2023-02-19 21:12:31 +0900

    Add LeetCode Solution - 219. Contains Duplicate II(read solution) https://leetcode.com/problems/contains-duplicate-ii/solutions/2727290/c-easy-detailed-explaination-optimized/?orderBy=most_votes

--- Array/219_Contains_Duplicate_II.h
+++ Array/219_Contains_Duplicate_II.h
@@ -2,23 +2,24 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
 
-        for (int i = 0; i <= nums.size(); i++)
-        {
-            for (int j = i + 1;  j <= i + k; j++)
-            {
-                if (nums.size() <= j)
-                {
-                    break;
-                }
+        unordered_map<int, int> m;
 
-                if (nums[i] == nums[j])
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.count(nums[i]))
+            {
+                if (i - m[nums[i]] <= k)
                 {
                     return true;
                 }
             }
+
+            m[nums[i]] = i;
         }
 
         return false;
-
     }
 };
+
+
+//https://leetcode.com/problems/contains-duplicate-ii/solutions/2727290/c-easy-detailed-explaination-optimized/?orderBy=most_votes
