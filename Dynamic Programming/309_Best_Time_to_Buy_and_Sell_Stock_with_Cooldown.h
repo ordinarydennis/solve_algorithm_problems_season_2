86751a766a155a7bb82f6eb34b828e7091984480
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-02-28 14:04:04 +0900

    Read LeetCode Solution - 714. Best Time to Buy and Sell Stock with Transaction Fee(read solution) https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108884/java-c-clean-code-dp-greedy/?orderBy=most_votes https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108871/2-solutions-2-states-dp-solutions-clear-explanation/?orderBy=most_votes https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108870/most-consistent-ways-of-dealing-with-the-series-of-stock-problems/?orderBy=most_votes

--- Dynamic Programming/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.h
+++ Dynamic Programming/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.h
@@ -1,57 +1,25 @@
 class Solution {
 public:
+	int maxProfit(vector<int>& p, int fee) {
+		int n = p.size();
 
-    // Recursive Soltion
-    // Time : O(2^N)
-    // Space : O(N)
+		if (n < 2) 
+			return 0;
 
-    int solveRec(vector<int>& prices, int day, bool buy) {
+		vector<int> buy(n, 0), sell(n, 0);
+		
+		buy[0] = -p[0];
+		
+		for (int i = 1; i < n; i++)
+		{
+			buy[i] = max(buy[i - 1], sell[i - 1] - p[i]); // keep the same as day i-1, or buy from sell status at day i-1
+			sell[i] = max(sell[i - 1], buy[i - 1] - fee + p[i]); // //keep the same as day i-1, or sell from buy status at day i-1
+		}
 
-        if (day >= prices.size()) {
-            return 0;
-        }
-
-        // First Choice
-        int ans1 = solveRec(prices, day + 1, buy); // no transaction this day
-
-
-        // Second Choice
-        int ans2 = 0;
-        // doing the required transaction this day
-        if (buy) {
-            ans2 = -prices[day] + solveRec(prices, day + 1, false);
-        }
-        else {
-            ans2 = prices[day] + solveRec(prices, day + 2, true);
-        }
-
-        return max(ans1, ans2);
-
-    }
-
-    int maxProfit(vector<int>& prices) {
-
-        int ans = 0;
-
-        // Recursive Solution 
-        ans = solveRec(prices, 0, true);
-
-        // Memoization Solution
-        // vector<vector<int>> memo(prices.size() + 2, vector<int>(2, -1));
-        // ans = solveMemo(prices, 0, true, memo);
-
-         // DP solution with O(N) space
-        // ans = solveDP1(prices);
-
-        // DP solution with O(1) space
-        //ans = solveDP2(prices);
-
-        return ans;
-
-    }
-
-
-
+		return sell[n - 1];
+	}
 };
 
-//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solutions/1521820/c-four-solutions-recursion-memoization-dp-with-o-n-space-dp-with-o-1-space/?orderBy=most_votes
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108884/java-c-clean-code-dp-greedy/?orderBy=most_votes
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108871/2-solutions-2-states-dp-solutions-clear-explanation/?orderBy=most_votes
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/solutions/108870/most-consistent-ways-of-dealing-with-the-series-of-stock-problems/?orderBy=most_votes
