0883ffcd5d9fad609abbb1eceadc4162802e428b
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-03-20 14:10:23 +0900

    Edit LeetCode Solution - 241. Different Ways to Add Parentheses(solved again with dp) https://leetcode.com/problems/different-ways-to-add-parentheses/solutions/66331/c-4ms-recursive-dp-solution-with-brief-explanation/?orderBy=most_votes

--- String/leetcode_241_Different_Ways_to_Add_Parentheses.h
+++ String/leetcode_241_Different_Ways_to_Add_Parentheses.h
@@ -1,51 +1,70 @@
 class Solution {
 
-	vector<int> ret;
+	/* if this type is unordered_map, occur complie error.
+	std::map 컨테이너는 std::pair<int, int> 타입을 key로 사용할 때 기본 생성자가 없어도 오류가 발생하지 않습니다. 
+	그 이유는 std::map은 레드-블랙 트리(Red-Black Tree) 자료구조를 사용하여 요소를 저장하기 때문입니다.
+	레드-블랙 트리는 노드의 삽입과 삭제를 할 때, 트리의 균형을 유지하기 위해 회전 연산을 수행합니다. 
+	따라서 노드의 삽입과 삭제는 항상 부모 노드와 자식 노드 간의 포인터 조작만으로 이루어지며, 키 타입의 기본 생성자를 호출할 필요가 없습니다.
+	반면에 std::unordered_map은 해시 테이블(Hash Table) 자료구조를 사용하여 요소를 저장합니다. 
+	해시 테이블은 키 값의 해시 함수를 사용하여 요소의 위치를 계산하는데, 
+	std::pair<int, int>와 같은 사용자 정의 타입의 경우 해시 함수가 제대로 구현되어 있지 않으면 컴파일 오류가 발생합니다. 
+	따라서 std::unordered_map은 키 타입에 대한 기본 생성자를 반드시 필요로 합니다.
+	*/
+	std::map<std::pair<int, int>, std::vector<int>> memo;
 
-	vector<int> calc(const string& expression, int begin, int end)
+	std::vector<int> calc(string& expression, int begin, int end)
 	{
-		vector<int> r;
+		auto findIt= memo.find(std::pair<int, int> {begin, end});
+
+		if (memo.end() != findIt)
+		{
+			return findIt->second;
+		}
+
+		std::vector<int> results;
 
 		string s;
-
 		for (int i = begin; i < end; i++)
 		{
-			if ('*' == expression[i] || '-' == expression[i] || '+' == expression[i])
+			char c = expression[i];
+			if ('+' == c || '-' == c || '*' == c)
 			{
-				auto llist = calc(expression, begin, i);
-				auto rlist = calc(expression, i + 1, end);
+				auto lList = calc(expression, begin, i);
+				auto rList = calc(expression, i + 1, end);
 
-				for (auto ln : llist)
+				for (auto l : lList)
 				{
-					for (auto rn : rlist)
+					for (auto r : rList)
 					{
-						if ('*' == expression[i])
+						if ('+' == c)
 						{
-							r.push_back(ln * rn);
+							results.push_back(l + r);
 						}
-						else if ('-' == expression[i])
+						else if ('-' == c)
 						{
-							r.push_back(ln - rn);
+							results.push_back(l - r);
 						}
 						else
 						{
-							r.push_back(ln + rn);
+							results.push_back(l * r);
 						}
 					}
-				}
+				}			
 			}
 			else
 			{
-				s.push_back(expression[i]);
+				s.push_back(c);
 			}
 		}
 
-		if (r.empty())
+		if (results.empty())
 		{
-			return vector<int> { stoi(s) };
+			results.push_back(stoi(s.c_str()));
 		}
-		
-		return r;
+
+		memo.emplace(std::pair<int, int> { begin, end }, results);
+
+		return results;
 	}
 
 
@@ -57,5 +76,5 @@ public:
 	}
 };
 
-//https://leetcode.com/problems/different-ways-to-add-parentheses/solutions/66331/c-4ms-recursive-dp-solution-with-brief-explanation/?orderBy=most_votes
 
+//https://leetcode.com/problems/different-ways-to-add-parentheses/solutions/66331/c-4ms-recursive-dp-solution-with-brief-explanation/?orderBy=most_votes
