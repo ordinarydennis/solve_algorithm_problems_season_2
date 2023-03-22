460385ff83baa2b840cdce9a313a6ae475988528
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-03-22 13:55:41 +0900

    Read LeetCode Solution - 306. Additive Number(read solution) https://leetcode.com/problems/additive-number/solutions/75576/0ms-concise-c-solution-perfectly-handles-the-follow-up-and-leading-0s/

--- String/leetcode_306_Additive_Number.h
+++ String/leetcode_306_Additive_Number.h
@@ -1,39 +1,55 @@
+
+//"199100199"
 class Solution {
-
-	void check(const string& num, int begin, int end, int sum, int count)
-	{
-
-		for (int i = 1, i < num.size(); i++)
-		{
-			string s = num.substr(begin, i);
-			
-			int n = std::stoi(s);
-
-			if (2 == count)
-			{
-				if (sum == n)
-				{
-					check(num, begin + 1, end, sum + n, count + 1);
-				}
-				else
-				{
-
-				}
-			}
-			else
-			{
-				check(num, begin + 1, end, sum + n, count + 1);
-			}
-		}
-
-	}
-
 public:
 	bool isAdditiveNumber(string num) {
 
+		for (int i = 1; i <= num.size() / 2; i++)
+		{
+			for (int j = 1; j <= (num.size() - i) / 2; j++)
+			{
+				if (check(num.substr(0, i), num.substr(i, j), num.substr(i + j)))
+					return true;
+			}
+		}
+		return false;
+	}
 
-		return check(num, 0, num.size(), 0, 0);
+	bool check(string num1, string num2, string num)
+	{
+		if (num1.size() > 1 && num1[0] == '0' || num2.size() > 1 && num2[0] == '0') 
+			return false;
 
+		string sum = add(num1, num2);
 
+		if (num == sum)
+			return true;
+
+		if (num.size() <= sum.size() || sum.compare(num.substr(0, sum.size())) != 0)
+			return false;
+
+		else 
+			return check(num2, sum, num.substr(sum.size()));
+	}
+
+	string add(string n, string m)
+	{
+		string res;
+
+		int i = n.size() - 1, j = m.size() - 1, carry = 0;
+
+		while (i >= 0 || j >= 0)
+		{
+			int sum = carry + (i >= 0 ? (n[i--] - '0') : 0) + (j >= 0 ? (m[j--] - '0') : 0);
+			res.push_back(sum % 10 + '0');
+			carry = sum / 10;
+		}
+
+		if (carry) 
+			res.push_back(carry + '0');
+
+		reverse(res.begin(), res.end());
+
+		return res;
 	}
 };
