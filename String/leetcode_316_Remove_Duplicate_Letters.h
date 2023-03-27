2c7cbe7e14f42479c4a24c016985cdfcf317b630
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-03-27 13:49:22 +0900

    Edit LeetCode Solution - 316. Remove Duplicate Letters(solved again)

--- String/leetcode_316_Remove_Duplicate_Letters.h
+++ String/leetcode_316_Remove_Duplicate_Letters.h
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    string removeDuplicateLetters(string s) {
+	string removeDuplicateLetters(string s) {
 
-        vector<int> lastIndex(26, 0);
+		std::unordered_map<char, int> m;
 
-        for (int i = 0; i < s.length(); i++) {
-            lastIndex[s[i] - 'a'] = i; // track the lastIndex of character presence
-        }
+		for (int i= 0; i < s.size(); i++)
+		{
+			m[s[i]] = i;
+		}
 
-        vector<bool> seen(26, false); // keep track seen
-        
-        stack<char> st;
+		std::stack<char> stk;
+		std::unordered_map<char, bool> seen;
 
-        for (int i = 0; i < s.size(); i++)
-        {
-            int curr = s[i] - 'a';
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (seen[s[i]])
+			{
+				continue;
+			}
 
-            if (seen[curr])
-                continue; // if seen continue as we need to pick one char only
+			char t = stk.top();
+			while ( stk.size() && s[i] < t && i < m[t] )
+			{
+				seen[t] = false;
+				stk.pop();
+			}
 
-            while (st.size() > 0 && st.top() > s[i] && i < lastIndex[st.top() - 'a'])
-            {
-                seen[st.top() - 'a'] = false; // pop out and mark unseen
-                st.pop();
-            }
-            st.push(s[i]); // add into stack
-            seen[curr] = true; // mark seen
-        }
 
-        string ans = "";
+			stk.push(s[i]);
+			seen[s[i]] = true;
+		}
 
-        while (st.size() > 0) {
-            ans += st.top();
-            st.pop();
-        }
+		string ret;
 
-        reverse(ans.begin(), ans.end());
+		while (stk.size())
+		{
+			ret += stk.top();
+			stk.pop();
+		}
 
-        return ans;
-    }
+		std::reverse(ret.begin(), ret.end());
+
+		return ret;
+	}
 };
-
-//https://leetcode.com/problems/remove-duplicate-letters/solutions/1859410/java-c-detailed-visually-explained/
