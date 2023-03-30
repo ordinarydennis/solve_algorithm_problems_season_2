e67d538eadb092132ac5c22a55fb9e9eef3f04ee
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-03-30 13:42:41 +0900

    Add LeetCode Solution - 637. Average of Levels in Binary Tree(10m)

---
+++ Tree/leetcode_637_Average_of_Levels_in_Binary_Tree.h
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+
+        vector<double> ret;
+
+        std::queue<TreeNode*> q;
+
+        q.push(root);
+
+        while (q.size())
+        {
+            int size = q.size();
+
+            double sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                auto* node = q.front();
+                q.pop();
+
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+                
+                sum += node->val;
+            }
+
+            ret.push_back(sum / size);
+        }
+
+        return ret;
+    }
+};
