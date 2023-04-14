1eafb90848d6a84cbff6a316676697f268c7f273
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-04-14 14:31:28 +0900

    Add LeetCode Solution - 813. Largest Sum of Averages(fail)

---
+++ Dynamic Programming/813_Largest_Sum_of_Averages.h
@@ -0,0 +1,34 @@
+class Solution {
+
+	double sumOfNums(vector<int>& nums, int startIndex, int count, int k)
+	{
+		if (k == count)
+		{
+			int sum = std::accumulate(nums.begin() + startIndex, nums.end(), 0);
+			double dsum = sum / (nums.size() - startIndex);
+			return dsum;
+		}
+
+		double max = 0;
+
+		for (int i = startIndex; i <= nums.size() - k; i++)
+		{
+			int sum = std::accumulate(nums.begin() + startIndex, nums.begin() + startIndex + i + 1, 0);
+
+			double dsum = sum / (i + 1);
+
+			double dSumNum = sumOfNums(nums, i + 1, count + 1, k);
+
+			max = std::max(max , dsum + dSumNum);
+		}
+
+		return max;
+	}
+
+public:
+	double largestSumOfAverages(vector<int>& nums, int k) {
+
+		return sumOfNums(nums, 0, 1, k);
+
+	}
+};
