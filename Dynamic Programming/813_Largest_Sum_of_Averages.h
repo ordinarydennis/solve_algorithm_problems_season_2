93e9c5e95453644cdbfd88e4ac7894e03f7b3f9e
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-04-17 15:17:17 +0900

    Edit LeetCode Solution - 813. Largest Sum of Averages(read solution) https://leetcode.com/problems/largest-sum-of-averages/solutions/126280/naive-detailed-step-by-step-approach-from-recursive-to-dp-o-n-solution/?orderBy=most_votes

--- Dynamic Programming/813_Largest_Sum_of_Averages.h
+++ Dynamic Programming/813_Largest_Sum_of_Averages.h
@@ -1,34 +1,41 @@
 class Solution {
 
-	double sumOfNums(vector<int>& nums, int startIndex, int count, int k)
+	double sumOfNums(vector<int>& nums, int k, std::vector<int>& sum, std::vector<std::vector<double>>& memo, int len, int s)
 	{
-		if (k == count)
+		if (memo[s][k] != 0) 
+			return memo[s][k];
+
+		if (k == 1)
 		{
-			double sum = std::accumulate(nums.begin() + startIndex, nums.end(), 0);
-			double dsum = sum / (nums.size() - startIndex);
-			return dsum;
+			memo[s][k] = ((double)(sum[len - 1] - sum[s] + nums[s]) / (len - s));
+			return memo[s][k];
 		}
 
-		double max = 0;
-
-		for (int i = startIndex; i < nums.size() - (k - count); i++)
+		for (int i = s; i + k <= len; i++)
 		{
-			double sum = std::accumulate(nums.begin() + startIndex, nums.begin() + startIndex + (i - startIndex) + 1, 0);
-
-			double dsum = sum / ((i - startIndex) + 1);
-
-			double dSumNum = sumOfNums(nums, i + 1, count + 1, k);
-
-			max = std::max(max, dsum + dSumNum);
+			memo[s][k] = std::max(
+				memo[s][k], 
+				((double)(sum[i] - sum[s] + nums[s]) / (i - s + 1)) + sumOfNums(nums, k - 1, sum, memo, len, i + 1)
+			);
 		}
-
-		return max;
+		return memo[s][k];
 	}
 
 public:
 	double largestSumOfAverages(vector<int>& nums, int k) {
 
-		return sumOfNums(nums, 0, 1, k);
+		std::vector<int> sum(nums.size(), 0);
+
+		std::vector<std::vector<double>> memo(nums.size(), std::vector<double>(k + 1, 0));
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			sum[i] = (0 < i) ? sum[i - 1] + nums[i] : nums[i];
+		}
+
+		return sumOfNums(nums, k, sum, memo, nums.size(), 0);
 
 	}
 };
+
+https://leetcode.com/problems/largest-sum-of-averages/solutions/126280/naive-detailed-step-by-step-approach-from-recursive-to-dp-o-n-solution/?orderBy=most_votes
