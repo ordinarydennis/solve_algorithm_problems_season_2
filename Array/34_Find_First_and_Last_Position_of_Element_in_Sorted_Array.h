3b489cefae1ee39336ba2cc48b8001a9dd786bbe
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-04-24 14:38:53 +0900

    Edit LeetCode Solution - 34. Find First and Last Position of Element in Sorted Array(read solution) https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/14699/clean-iterative-solution-with-two-binary-searches-with-explanation/

--- Array/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.h
+++ Array/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.h
@@ -1,41 +1,47 @@
 class Solution {
 
-    std::set<int> ss;
-
-    void search(std::vector<int>& nums, int start, int end, int target)
-    {
-        if (end < start)
-        {
-            return;
-        }
-
-        int middle = (start + end) / 2;
-
-        if (target == nums[middle])
-        {
-            ss.insert(middle);
-        }
-
-        if (nums[middle] < target)
-        {
-            search(nums, middle + 1, end, target);
-        }
-        else
-        {
-            search(nums, start, middle, target);
-        }
-
-    }
-
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
 
-        search(nums, 0, nums.size(), target);
+		vector<int> ret(2, -1);
 
-        std::vector<int> ret;
+		if (0 == nums.size())
+		{
+			return ret;
+		}
 
-       // std::vector<int> ret = { (*ss.begin()), *(ss.rbegin()) };
+		int i = 0;
+		int j = nums.size() - 1;
 
-        return ret;
+		// Search for the left one
+		while (i < j)
+		{
+			int mid = (i + j) / 2;
+			if (nums[mid] < target)
+				i = mid + 1;
+			else 
+				j = mid;
+		}
+
+		if (nums[i] != target)
+			return ret;
+		else
+			ret[0] = i;
+
+		// Search for the right one
+		j = nums.size() - 1;  // We don't have to set i to 0 the second time.
+
+		while (i < j)
+		{
+			int mid = (i + j) / 2 + 1;	// Make mid biased to the right
+
+			if (nums[mid] > target)
+				j = mid - 1;
+			else 
+				i = mid;				// So that this won't make the search range stuck.
+		}
+		ret[1] = j;
+
+		return ret;
     }
 };
