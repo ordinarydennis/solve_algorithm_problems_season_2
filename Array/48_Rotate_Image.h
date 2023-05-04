ab5c72e885035fed6d6cf272c4c9e30258b95c5f
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-05-04 14:39:26 +0900

    Edit LeetCode Solution - 48. Rotate Image(solved again, fail)

--- Array/48_Rotate_Image.h
+++ Array/48_Rotate_Image.h
@@ -1,17 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int a = 0;
-        int b = n - 1;
-        while (a < b) {
-            for (int i = 0;i < (b - a);++i) {
-                swap(matrix[a][a + i], matrix[a + i][b]);
-                swap(matrix[a][a + i], matrix[b][b - i]);
-                swap(matrix[a][a + i], matrix[b - i][a]);
+
+        int rSize = matrix.size();
+
+        int cSize = matrix[0].size();
+
+        int start = 0;
+
+        for (int r = 0; r < rSize; r++)
+        {
+            for (int i = start; i < cSize - start; i++)
+            {
+                std::swap(matrix[r][i], matrix[r + i][cSize - start - 1]);
+                std::swap(matrix[r][i], matrix[rSize - start - 1][cSize - i - start - 1]);
+                std::swap(matrix[r][i], matrix[rSize - i - start - 1][i + start]);
             }
-            ++a;
-            --b;
+            start++;
         }
     }
 };
