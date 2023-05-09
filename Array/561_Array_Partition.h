8dc156fde312cd698e5cb50c84b627071403a5e6
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-05-09 20:48:06 +0900

    Add LeetCode Solution  - 561. Array Partition(10m)

---
+++ Array/561_Array_Partition.h
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int arrayPairSum(vector<int>& nums) {
+
+		sort(nums.begin(), nums.end());
+
+		int ret = 0;
+
+		for (int i = 0; i < nums.size();)
+		{
+			ret += nums[i];
+			i += 2;
+		}
+
+		return ret;
+	}
+};
