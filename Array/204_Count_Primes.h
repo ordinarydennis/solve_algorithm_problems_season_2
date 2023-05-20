3062e4e87589971bd5f15a1ea8637e5c8f1473e9
ordinarydennis <ordinarydennis@gmail.com>
2023-05-20 23:05:51 +0900

    Edit LeetCode Solution  - 204. Count Primes(read solution) https://leetcode.com/problems/count-primes/solutions/1200796/js-python-java-c-easy-sieve-of-eratosthenes-solution-w-explanation/

--- Array/204_Count_Primes.h
+++ Array/204_Count_Primes.h
@@ -2,32 +2,30 @@ class Solution {
 public:
 	int countPrimes(int n) {
 
-		if (n <= 2)
-			return 0;
-
-		if (n <= 3)
-			return 1;
-
-		std::vector<int> primes = { 2, 3 };
-
-		for (int i = 5; i < n; i++)
+		if (0 == n || 1 == n)
 		{
-			int j = 0;
+			return 0;
+		}
 
-			for (; j < primes.size(); j++)
-			{
-				if (i % primes[j] == 0)
-				{
-					break;
-				}
-			}
+		std::vector<int> sieve(n, 1);
 
-			if (primes.size() == j)
+		int count = 0;
+
+		for (int i = 2; i < n; i++)
+		{
+			if (0 == sieve[i])
+				continue;
+
+			count++;
+
+			for (int j = 2; i * j < n; j++)
 			{
-				primes.push_back(i);
+				sieve[i * j] = 0;
 			}
 		}
 
-		return primes.size();
+		return count;
 	}
 };
+
+//https://leetcode.com/problems/count-primes/solutions/1200796/js-python-java-c-easy-sieve-of-eratosthenes-solution-w-explanation/
