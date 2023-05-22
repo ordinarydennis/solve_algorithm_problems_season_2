4507ffc76ad9d82e89ac23721d33b955096752e0
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-05-22 14:27:04 +0900

    Edit LeetCode Solution  - 179. Largest Number(Read Solution) https://leetcode.com/problems/largest-number/solutions/53158/my-java-solution-to-share/

--- Array/179_Largest_Number.h
+++ Array/179_Largest_Number.h
@@ -8,56 +8,19 @@ public:
 			string lstr = to_string(l);
 			string rstr = to_string(r);
 
-			int i = 0;
-			int j = 0;
+			std::string s1 = lstr + rstr;
+			std::string s2 = rstr + lstr;
 
-			int ret = 0;
+			return 0 < s1.compare(s2);
 
-			while (i < lstr.size() && j < rstr.size())
-			{
-				if (lstr[i] == rstr[j])
-				{
-					i++;
-					j++;
-					continue;
-				}
-
-				if (rstr[j] < lstr[i])
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-			}
-
-			if (lstr.size() < rstr.size())
-			{
-				if ('0' == rstr[j])
-				{
-					return true;
-				}
-				else
-					return false;
-			}
-			else
-			{
-				if ('0' == lstr[i])
-				{
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-			
-			return lstr.size() < rstr.size();
-			
 			}
 		);
 
+		if (0 == nums[0])
+		{
+			return "0";
+		}
+		
 		std::string ret;
 
 		for (int n : nums)
@@ -68,3 +31,5 @@ public:
 		return ret;
 	}
 };
+
+//https://leetcode.com/problems/largest-number/solutions/53158/my-java-solution-to-share/
