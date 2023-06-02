debe268f57c5fa80e72ba5f8ce1431e9ec44dc72
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-06-02 14:23:17 +0900

    Edit LeetCode Soltuion  - 287. Find the Duplicate Number(read solution) https://leetcode.com/problems/find-the-duplicate-number/solutions/1892921/9-approaches-count-hash-in-place-marked-sort-binary-search-bit-mask-fast-slow-pointers/
    https://leetcode.com/problems/find-the-duplicate-number/solutions/1892872/c-algorithm-4-approaches-binary-search-brute-force-cnt-array-map/

--- Array/287_Find_the_Duplicate_Number.h
+++ Array/287_Find_the_Duplicate_Number.h
@@ -27,3 +27,36 @@ public:
 
 	}
 };
+
+//refer to solution APPROACH 1 USING BINARY SEARCH
+https://leetcode.com/problems/find-the-duplicate-number/solutions/1892872/c-algorithm-4-approaches-binary-search-brute-force-cnt-array-map/
+class Solution {
+public:
+	int findDuplicate(vector<int>& nums) {
+		int low = 1, high = nums.size() - 1, cnt;
+
+		while (low <= high)
+		{
+			int mid = low + (high - low) / 2;
+			cnt = 0;
+			// cnt number less than equal to mid
+			for (int n : nums)
+			{
+				if (n <= mid)
+					++cnt;
+			}
+			// binary search on left
+			if (cnt <= mid)
+				low = mid + 1;
+			else
+				// binary search on right
+				high = mid - 1;
+
+		}
+		return low;
+	}
+	// for github repository link go to my profile.
+
+};
+//9 Approaches: Count, Hash, In - place Marked, Sort, Binary Search, Bit Mask, Fast Slow Pointers
+https://leetcode.com/problems/find-the-duplicate-number/solutions/1892921/9-approaches-count-hash-in-place-marked-sort-binary-search-bit-mask-fast-slow-pointers/
