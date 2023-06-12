9e9dbb89d449efa6fc9ddb90056fc798efba79f7
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-06-12 14:30:35 +0900

    Edit LeetCode Solution - 697. Degree of an Array(retry and fail)

--- Array/697_Degree_of_an_Array.h
+++ Array/697_Degree_of_an_Array.h
@@ -2,5 +2,45 @@ class Solution {
 public:
 	int findShortestSubArray(vector<int>& nums) {
 
+		std::unordered_map<int, int> m;
+
+		int max = INT_MIN;
+
+		for (int n : nums)
+		{
+			m[n]++;
+			max = std::max(m[n], max);
+		}
+
+		int ret = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int min = INT_MAX;
+
+			m.clear();
+
+			for (int j = i; j < nums.size(); j++)
+			{
+				m[j]++;
+
+				if (max == m[j])
+				{
+					if (max == j - i + 1)
+					{
+						return max;
+					}
+					else
+					{
+						min = std::min(min, j - i + 1);
+						ret = min;
+						break;
+					}
+				}
+			}
+		}
+
+		return ret;
+
 	}
 };
