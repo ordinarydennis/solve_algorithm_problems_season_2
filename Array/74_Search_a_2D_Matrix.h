1bede7ead7d679e82c308da15d426da4760688b2
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-06-16 14:42:29 +0900

    Edit LeetCode Solution - 74. Search a 2D Matrix(read solution)

--- Array/74_Search_a_2D_Matrix.h
+++ Array/74_Search_a_2D_Matrix.h
@@ -1,3 +1,38 @@
+//solution
+//https://leetcode.com/problems/search-a-2d-matrix/solutions/26219/binary-search-on-an-ordered-matrix/
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+		int row_num = matrix.size();
+		int col_num = matrix[0].size();
+
+		int begin = 0, end = row_num * col_num - 1;
+
+		while (begin <= end)
+		{
+			int mid = (begin + end) / 2;
+
+			int mid_value = matrix[mid / col_num][mid % col_num];
+
+			if (mid_value == target)
+			{
+				return true;
+			}
+			else if (mid_value < target)
+			{
+				begin = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+
+		return false;
+	}
+};
+
 class Solution {
 public:
 	bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -24,7 +59,6 @@ public:
 
 	}
 };
-
 
 class Solution {
 public:
