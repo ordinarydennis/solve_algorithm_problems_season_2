2b36bd879fe4e930735c9d67cf8a4f83b9f5b0f8
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-06-21 14:35:44 +0900

    Edit LeetCode Solution - 744. Find Smallest Letter Greater Than Target(read solution https://leetcode.com/problems/find-smallest-letter-greater-than-target/solutions/760959/c-3-approaches-easy-to-understand/)

--- Array/744_Find_Smallest_Letter_Greater_Than_Target.h
+++ Array/744_Find_Smallest_Letter_Greater_Than_Target.h
@@ -16,3 +16,39 @@ public:
 		return letters[s];
 	}
 };
+
+class Solution {
+public:
+	char nextGreatestLetter(vector<char>& letters, char target) {
+
+		int low = 0;
+		int high = letters.size() - 1;
+
+		if (letters[low] > target)
+		{
+			return letters[low];
+		}
+		
+		if (letters[high] < target)
+		{
+			return letters[low];
+		}
+		
+		while (low < high) {
+
+			int mid = (low + high) / 2;
+
+			if (letters[mid] <= target)
+			{
+				low = mid + 1;
+			}
+			else
+			{
+				high = mid;
+			}
+		}
+		return letters[low] > target ? letters[low] : letters[0];
+	}
+};
+
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/solutions/760959/c-3-approaches-easy-to-understand/
