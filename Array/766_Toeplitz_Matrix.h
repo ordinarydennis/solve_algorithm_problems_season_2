3175e96b1853f0c2cb40c40662addd409667eddd
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-06-23 14:25:42 +0900

    Add LeetCode Solution - 766. Toeplitz Matrix(14m)

---
+++ Array/766_Toeplitz_Matrix.h
@@ -0,0 +1,35 @@
+class Solution {
+public:
+	bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+
+		int maxX = matrix[0].size() - 1;
+		int maxY = matrix.size() - 1;
+
+
+		for (int y = 0; y <= maxY; y++)
+		{
+			for (int x = 0; x <= maxX; x++)
+			{
+				int itY = y;
+				int itX = x;
+
+				int pre = matrix[itY][itX];
+
+				while (itY <= maxY && itX <= maxX)
+				{
+					if (pre != matrix[itY][itX])
+					{
+						return false;
+					}
+					
+
+					itY++;
+					itX++;
+				}
+				
+			}
+		}
+
+		return true;
+	}
+};
