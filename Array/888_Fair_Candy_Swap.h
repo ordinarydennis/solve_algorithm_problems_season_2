2fa325cafb3307a2bc54b0356ca68ee168dad10f
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-07-02 07:10:00 +0900

    Edit LeetCode Solution - 888. Fair Candy Swap(read solution) https://leetcode.com/problems/fair-candy-swap/solutions/161269/c-java-python-straight-forward/

--- Array/888_Fair_Candy_Swap.h
+++ Array/888_Fair_Candy_Swap.h
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+	vector<int> fairCandySwap(vector<int> A, vector<int> B) {
 
-    }
+		int dif = (accumulate(A.begin(), A.end(), 0) - accumulate(B.begin(), B.end(), 0)) / 2;
+
+		unordered_set<int> S(A.begin(), A.end());
+
+		for (int b : B)
+		{
+			if (S.count(b + dif))
+			{
+				return { b + dif, b };
+			}
+		}
+
+		return {};
+			
+	}
 };
+
+//read solution
+//https://leetcode.com/problems/fair-candy-swap/solutions/161269/c-java-python-straight-forward/
