4094e8991df3bae57181e20849d2305691a6a833
ordinarydennis <ordinarydennis@gmail.com>
2023-07-06 19:14:11 +0900

    Edit LeetCode Solution - 905. Sort Array By Parity(read solution) - a shorter code

--- Array/905_Sort_Array_By_Parity.h
+++ Array/905_Sort_Array_By_Parity.h
@@ -2,14 +2,11 @@ class Solution {
 public:
 	vector<int> sortArrayByParity(vector<int>& nums) {
 
-		int oI = 0;
-
 		for (int oI = 0, i = 0; i < nums.size(); i++)
-		{
-			if (0 == nums[i] % 2)
-				std::swap(nums[i], nums[oI++]);
-		}
+			if (0 == nums[i] % 2) std::swap(nums[i], nums[oI++]);
 
 		return nums;
 	}
 };
+
+//https://leetcode.com/problems/sort-array-by-parity/solutions/170734/c-java-in-place-swap/
