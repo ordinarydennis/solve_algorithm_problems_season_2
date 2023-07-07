d7a7ebe040c6b14cb6ce04769ceb53a756bb8a92
ordinarydennis <ordinarydennis@gmail.com>
2023-07-07 21:59:58 +0900

    Add LeetCode Solution - 908. Smallest Range I(25m)

---
+++ Array/908_Smallest_Range_I.h
@@ -0,0 +1,27 @@
+class Solution {
+public:
+	int smallestRangeI(vector<int>& nums, int k) {
+
+		std::sort(nums.begin(), nums.end());
+
+		int i = 0;
+
+		while (1)
+		{
+			int a = nums[nums.size() - 1] - k + i;
+			int b = nums[0] + k;
+
+			if (a - b < 0)
+			{
+				i++;
+			}
+			else
+			{
+				return a - b;
+			}
+			
+		}
+		
+		return 0;
+	}
+};
