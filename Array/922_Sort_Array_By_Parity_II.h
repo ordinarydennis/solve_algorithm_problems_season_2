460a4159dd1ace15e90dbf3cb3602939118aac5f
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-07-11 12:53:26 +0900

    Edit LeetCode Solution - 922. Sort Array By Parity II(read solution)

--- Array/922_Sort_Array_By_Parity_II.h
+++ Array/922_Sort_Array_By_Parity_II.h
@@ -1,27 +1,41 @@
-//solution
+//my solution
 class Solution {
 public:
-	vector<int> sortArrayByParityII(vector<int>& nums) {
+	vector<int> sortArrayByParityII(vector<int>& A) {
 
-		int ei = 0;
-		int oi = 1;
-
-		std::vector<int> ret(nums.size());
-
-		for (int i = 0; i <nums.size(); i++)
+		for (int i = 0, j = 1; i < A.size(); i += 2, j += 2)
 		{
-			if (nums[i] % 2 == 0)
-			{
-				ret[ei] = nums[i];
-				ei += 2;
-			}
-			else
-			{
-				ret[oi] = nums[i];
-				oi += 2;
-			}
-		}
+			while (i < A.size() && A[i] % 2 == 0)
+				i += 2;
 
-		return ret;
+			while (j < A.size() && A[j] % 2 == 1)
+				j += 2;
+
+			if (i < A.size())
+				swap(A[i], A[j]);
+		}
+		return A;
 	}
+
 };
+
+//solution
+//https://leetcode.com/problems/sort-array-by-parity-ii/solutions/181158/c-5-lines-two-pointers-2-liner-bonus/
+vector<int> sortArrayByParityII(vector<int>& A) {
+
+	for (int i = 0, j = 1; i < A.size(); i += 2, j += 2)
+	{
+		while (i < A.size() && A[i] % 2 == 0) 
+			i += 2;
+
+		while (j < A.size() && A[j] % 2 == 1)
+			j += 2;
+
+		//i´Â Â¦¼öÁö¸¸ È¦¼ö°¡ ³ª¿Â ÀÎµ¦½º
+		//j´Â È¦¼öÁö¸¸ Â¦¼ö°¡ ³ª¿Â ÀÎµ¦½º
+		//µÑÀÌ Ä¡È¯
+		if (i < A.size()) 
+			swap(A[i], A[j]);
+	}
+	return A;
+}
