68debc775edf2efe153a5bee9b5432490239f032
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-07-13 12:56:45 +0900

    Add LeetCode Solution - 961. N-Repeated Element in Size 2N Array(read solution) https://leetcode.com/problems/n-repeated-element-in-size-2n-array/solutions/208563/java-c-python-o-1-solution/ - 976. Largest Perimeter Triangle(10m)

--- Array/961_N-Repeated_Element_in_Size_2N_Array.h
+++ Array/961_N-Repeated_Element_in_Size_2N_Array.h
@@ -2,16 +2,16 @@ class Solution {
 public:
 	int repeatedNTimes(vector<int>& nums) {
 
-		int n = nums.size() / 2;
-
-		std::unordered_map<int,int> map;
+		std::unordered_set<int> set;
 
 		for (int i = 0; i < nums.size(); i++)
 		{
-			if (++map[nums[i]] == n)
+			if (set.count(nums[i]))
 			{
 				return nums[i];
 			}
+
+			set.insert(nums[i]);
 		}
 
 		return 0;
