119a5f9fbd458e282635beac1896be543cdceb37
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-08-01 12:54:46 +0900

    Add LeetCode Solution - 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree(8m) - read solution https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/solutions/538009/c-minimalizm/

---
+++ Tree/leetcode_1379_Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.h
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+
+    TreeNode* ret = nullptr;
+
+    void traversal(TreeNode* cloned, TreeNode* target)
+    {
+        if (nullptr == cloned)
+        {
+            return;
+        }
+
+        if (ret)
+        {
+            return;
+        }
+
+        traversal(cloned->left, target);
+
+        if (target->val == cloned->val)
+        {
+            ret = cloned;
+            return;
+        }
+
+        traversal(cloned->right, target);
+    }
+
+
+public:
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+
+        traversal(cloned, target);
+
+        return ret;
+
+    }
+};
+
+
+//solution
+//https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/solutions/538009/c-minimalizm/
+class Solution {
+
+public:
+	TreeNode* getTargetCopy(TreeNode* orig, TreeNode* clone, TreeNode* targ) {
+
+		return orig == nullptr ? nullptr :
+			orig == targ ? clone :
+			getTargetCopy(orig->left, clone->left, targ) ? : getTargetCopy(orig->right, clone->right, targ);
+
+	}
+};
