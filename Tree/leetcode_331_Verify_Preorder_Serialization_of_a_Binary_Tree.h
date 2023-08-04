aeb04e1d4d7c70ab17630a54531f12e051029c9e
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-08-04 13:35:01 +0900

    Edit LeetCode Solution - 331. Verify Preorder Serialization of a Binary Tree(read solution) https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/solutions/78605/simple-c-solution-using-stack-without-extra-splitting-function/

--- Tree/leetcode_331_Verify_Preorder_Serialization_of_a_Binary_Tree.h
+++ Tree/leetcode_331_Verify_Preorder_Serialization_of_a_Binary_Tree.h
@@ -1,35 +1,39 @@
 class Solution {
 public:
 	bool isValidSerialization(string preorder) {
+		stack<char> stk;
+		bool isNum = false;
+		preorder.push_back(','); // dummy tail
 
-
-		std::stack<int> st;
-
-		for (int i = 0; i < preorder.size() - 2; i++)
-		{
-			if (',' == preorder[i] || '#' == preorder[i])
+		for (auto c : preorder) {
+			if (c == '#')
 			{
-				continue;
+				// absorb: search for pattern `#, number` backward
+				while (!stk.empty() && stk.top() == '#') {
+					stk.pop(); // pop `#`
+
+					if (stk.empty() || stk.top() == '#')
+						return false; // pattern `#,#,#`
+
+					stk.pop(); // pop `number`
+				}
+				stk.push('#'); // replace `number` with `#` since it has been fully explored/validated
 			}
-
-			st.push(preorder[i]);
-
-			//leaf
-			if ('#' == preorder[i + 1] && '#' == preorder[i + 2])
+			else if (c == ',')
 			{
-				if (st.size())
-				{
-					st.pop();
-				}
-				else
-				{
-					return false;
-				}
-				i += 2;
+				if (isNum)
+					stk.push('n'); // indicate this is a number instead of using the real number
+				isNum = false;
+			}
+			else {
+				isNum = true;
 			}
 		}
 
-		return st.size();
-
+		return stk.size() == 1 && stk.top() == '#';
 	}
 };
+
+//encounting '#' character twice, top of the stack value is last node.
+//so the last node can be replace '#', because last node means end of tree.
+//https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/solutions/78605/simple-c-solution-using-stack-without-extra-splitting-function/
