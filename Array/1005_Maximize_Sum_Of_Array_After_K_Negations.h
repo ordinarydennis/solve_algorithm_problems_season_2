228ac0cec168e2d37ab6848b9210c9de5aa9029a
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-08-07 13:43:42 +0900

    Edit LeetCode Solution - 1005. Maximize Sum Of Array After K Negations(retry and fail)

--- Array/1005_Maximize_Sum_Of_Array_After_K_Negations.h
+++ Array/1005_Maximize_Sum_Of_Array_After_K_Negations.h
@@ -1,13 +1,54 @@
 class Solution {
 public:
-	int largestSumAfterKNegations(vector<int>& A, int K) {
-		sort(A.begin(), A.end());
-		for (int i = 0; K > 0 && i < A.size() && A[i] < 0; ++i, --K)
-			A[i] = -A[i];
-		return accumulate(A.begin(), A.end(), 0) - (K % 2) * *min_element(A.begin(), A.end()) * 2;
+	int largestSumAfterKNegations(vector<int>& nums, int k) {
+
+		std::multimap<int, int> m;
+
+		for (int i = 0; i  < nums.size(); i++)
+		{
+			m.emplace(nums[i], i);
+		}
+
+
+		for (const auto& [n, index] : m)
+		{
+			if (n < 0)
+			{
+				k--;
+				nums[index] = -nums[index];
+			}
+			else if (0 == n)
+			{
+				break;
+			}
+			else
+			{
+				k = k % 2;
+				//positive..
+				if (1 <= k)
+				{
+					k--;
+					nums[index] = -nums[index];
+				}
+			}
+
+			if (k <= 0)
+			{
+				break;
+			}
+		}
+
+		for (auto [n, inde] : m)
+		{
+			cout << n << " ";
+		}
+		cout << endl;
+		for (auto n : nums)
+		{
+			cout << n << " ";
+		}
+
+		return std::accumulate(nums.begin(), nums.end(), 0);
+
 	}
 };
-
-
-//read solution
-//leetcode.com/problems/maximize-sum-of-array-after-k-negations/solutions/252254/java-c-python-sort/
