afc73c5aa0b466a9746be59338935254f4c6b17e
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-08-09 12:48:03 +0900

    Edit LeetCode Solution -  977. Squares of a Sorted Array(resolved, 15m)

--- Array/977_Squares_of_a_Sorted_Array.h
+++ Array/977_Squares_of_a_Sorted_Array.h
@@ -2,13 +2,30 @@ class Solution {
 public:
 	vector<int> sortedSquares(vector<int>& nums) {
 
-		int l = 0, r = nums.size() - 1, i = nums.size() - 1;
+		vector<int> ret(nums.size(), 0);
 
-		std::vector<int> ret(nums.size());
-		
+		int i = nums.size() - 1;
+		int l = 0;
+		int r = nums.size() - 1;
+
 		while (l <= r)
-			ret[i--] = abs(nums[l]) < abs(nums[r]) ? nums[r] * nums[r--] : nums[l] * nums[l++];
+			ret[i--] = (nums[l] * nums[l] < nums[r] * nums[r]) ? nums[r] * nums[r--] : nums[l] * nums[l++];
 
 		return ret;
+	}
+};
+
+class Solution {
+public:
+	vector<int> sortedSquares(vector<int>& nums) {
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			nums[i] = nums[i] * nums[i];
+		}
+
+		std::sort(nums.begin(), nums.end());
+
+		return nums;
 	}
 };
