457825962f25158eb318a076c756ae17e2834a57
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-08-21 13:01:56 +0900

    Edit LeetCode Solution - 337. House Robber III(read solution) https://leetcode.com/problems/house-robber-iii/solutions/79330/step-by-step-tackling-of-the-problem/

--- Depth-First-Search/337_House_Robber_III.h
+++ Depth-First-Search/337_House_Robber_III.h
@@ -1,56 +1,57 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Time Limit Exceeded
 class Solution {
 public:
 	int rob(TreeNode* root) {
-
-		if (nullptr == root)
-		{
+		if (root == nullptr)
 			return 0;
+
+		int val = 0;
+
+		if (root->left != nullptr) {
+			val += rob(root->left->left) + rob(root->left->right);
 		}
 
-		std::queue<TreeNode*> q;
-
-		q.push(root);
-
-		int count = 0;
-
-		int sum[2] = { 0, 0 };
-
-		while (q.size())
-		{
-			int s = q.size();
-			for (int i = 0; i < s; i++)
-			{
-				auto* node = q.front();
-
-				sum[count % 2] += node->val;
-
-				if (node->left)
-				{
-					q.push(node->left);
-				}
-				if (node->right)
-				{
-					q.push(node->right);
-				}
-
-				q.pop();
-			}
-			
-			count++;
+		if (root->right != nullptr) {
+			val += rob(root->right->left) + rob(root->right->right);
 		}
-		
-		return std::max(sum[0], sum[1]);
+
+		return std::max(val + root->val, rob(root->left) + rob(root->right));
 	}
+
 };
 
+class Solution {
+public:
+	int rob(TreeNode* root) {
+		map<TreeNode*, int> map;
+		return robSub(root, map);
+	}
+
+private:
+	int robSub(TreeNode* root, map<TreeNode*, int>& map)
+	{
+		if (root == nullptr)
+			return 0;
+
+		if (map.count(root))
+			return map[root];
+
+		int val = 0;
+
+		if (root->left != nullptr)
+		{
+			val += robSub(root->left->left, map) + robSub(root->left->right, map);
+		}
+
+		if (root->right != nullptr) {
+			val += robSub(root->right->left, map) + robSub(root->right->right, map);
+		}
+
+		val = std::max(val + root->val, robSub(root->left, map) + robSub(root->right, map));
+		map.emplace(root, val);
+
+		return val;
+	}
+
+};
+//https://leetcode.com/problems/house-robber-iii/solutions/79330/step-by-step-tackling-of-the-problem/
