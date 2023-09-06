7b6ab7f335d101e95a5bac8094e0364942474316
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-09-06 13:31:01 +0900

    Edit LeetCode Solution - 653. Two Sum IV - Input is a BST(read solution) https://leetcode.com/problems/two-sum-iv-input-is-a-bst/solutions/106063/c-clean-code-o-n-time-o-lg-n-space-binarytree-iterator/

---
+++ Depth-First-Search/653_Two_Sum_IV_-_Input_is_a_BST.h
@@ -0,0 +1,112 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+	std::vector<int> v;
+
+	void dfs(TreeNode* root, std::vector<int>& v)
+	{
+		if (nullptr == root)
+			return;
+
+		dfs(root->left, v);
+
+		v.push_back(root->val);
+
+		dfs(root->right, v);
+	}
+
+public:
+	bool findTarget(TreeNode* root, int k) {
+
+		dfs(root, v);
+
+		int l = 0;
+		int r = v.size() - 1;
+
+		while (l < r)
+		{
+			if (k == v[l] + v[r])
+			{
+				return true;
+			}
+
+			if (k < v[l] + v[r])
+			{
+				r--;
+			}
+			else
+			{
+				l++;
+			}
+		}
+
+		return false;
+	}
+};
+
+class BSTIterator {
+	stack<TreeNode*> s;
+	TreeNode* node;
+	bool forward;
+	int cur;
+public:
+	BSTIterator(TreeNode* root, bool forward) : node(root), forward(forward) 
+	{ 
+		(*this)++; 
+	};
+
+	int operator*() { return cur; }
+
+
+	void operator++(int) {
+		while (node || !s.empty()) {
+			if (node) {
+				s.push(node);
+				node = forward ? node->left : node->right;
+			}
+			else {
+				node = s.top();
+				s.pop();
+				cur = node->val;
+				node = forward ? node->right : node->left;
+				break;
+			}
+		}
+	}
+};
+
+class Solution {
+public:
+	bool findTarget(TreeNode* root, int k) {
+		if (!root) 
+			return false;
+
+		BSTIterator l(root, true);
+
+		BSTIterator r(root, false);
+
+		while (*l < *r) {
+			if (*l + *r == k) 
+				return true;
+			else if 
+				(*l + *r < k) 
+				l++;
+			else 
+				r++;
+		}
+		return false;
+	}
+};
+
+//BST Iterator
+//https://leetcode.com/problems/two-sum-iv-input-is-a-bst/solutions/106063/c-clean-code-o-n-time-o-lg-n-space-binarytree-iterator/
