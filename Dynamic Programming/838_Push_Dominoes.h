3f74a82538fa221a05550a2aec3bd187a5a73618
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-09-27 14:25:02 +0900

    Edit LeetCode Solution - 838. Push Dominoes(read solution) https://leetcode.com/problems/push-dominoes/solutions/132332/java-c-python-two-pointers/

--- Dynamic Programming/838_Push_Dominoes.h
+++ Dynamic Programming/838_Push_Dominoes.h
@@ -1,6 +1,55 @@
 class Solution {
 public:
-	string pushDominoes(string dominoes) {
+	string pushDominoes(string s) {
 
+		int right = -1;
+		int end = s.size();
+
+		for (int i = 0; i < end; i++)
+		{
+			if ('L' == s[i])
+			{
+				if (right == -1)
+				{
+					for (int L = i - 1; 0 < L && s[L] == '.'; L--)
+					{
+						s[L] = 'L';
+					}
+				}
+				else
+				{
+					for (int L = i - 1, R = right + 1; s[L] == '.' && R < L; L--, R++)
+					{
+						s[L] = 'L';
+						s[R] = 'R';
+					}
+					right = -1;
+				}
+			}
+			else if ('R' == s[i])
+			{
+				if (right != -1)
+				{
+					for (int R = right + 1; R < i; R++)
+					{
+						s[R] = 'R';
+					}
+				}
+				right = i;
+			}
+		}
+
+		if (-1 != right)
+		{
+			for (int R = right + 1; R < end; R++)
+			{
+				s[R] = 'R';
+			}
+		}
+
+
+		return s;
 	}
 };
+
+//https://leetcode.com/problems/push-dominoes/solutions/132332/java-c-python-two-pointers/
