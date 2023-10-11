c7cb2d9a3fa64f956a27f2e5b76317f2989c9aac
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-10-11 21:40:28 +0900

    Edit LeetCode Solution - 845. Longest Mountain in Array(read solution) https://leetcode.com/problems/longest-mountain-in-array/editorial/

--- Dynamic Programming/845_Longest_Mountain_in_Array.h
+++ Dynamic Programming/845_Longest_Mountain_in_Array.h
@@ -1,75 +1,33 @@
-Input: arr = [2, 1, 4, 7, 3, 2, 5]
-Output : 5
-
 class Solution {
 public:
 	int longestMountain(vector<int>& arr) {
 
+		int max = static_cast<int>(arr.size());
+
+		int start = 0;
+		int end = 0;
+
 		int ret = 0;
 
-
-		for (int i = 0; i < arr.size(); i++)
+		while (start < max)
 		{
-			int preL = INT_MAX;
-			int lIndex = i - 1;
-			preL = arr[i];
+			end = start;
 
-			int preR = INT_MAX;
-			int rIndex = i + 1;
-			preR = arr[i];
-
-			int count = 0;
-
-			bool exitLoopL = false;
-			bool exitLoopR = false;
-
-			while (true)
+			if (end + 1 < max && arr[end] < arr[end + 1])
 			{
-				if (0 <= lIndex)
-				{
-					if (preL > arr[lIndex])
-					{
-						lIndex--;
-						preL = arr[lIndex];
-						count++;
-					}
-					else
-					{
-						exitLoopL = true;
-					}
-				}
-				else
-				{
-					exitLoopL = true;
-				}
+				while (end + 1 < max && arr[end] < arr[end + 1]) end++;
 
-				if (rIndex < arr.size() - 1)
+				if (end + 1 < max && arr[end] > arr[end + 1])
 				{
-					if (preR > arr[rIndex])
-					{
-						rIndex++;
-						preR = arr[rIndex];
-						count++;
-					}
-					else
-					{
-						exitLoopR = true;
-					}
-				}
-				else
-				{
-					exitLoopR = true;
-				}
+					while (end + 1 < max && arr[end] > arr[end + 1]) end++;
 
-				if (exitLoopL && exitLoopR)
-				{
-					break;
+					ret = std::max(ret, end - start + 1);
 				}
 			}
 
-			ret = std::max(ret, count);
+			start = std::max(end, start + 1);
 		}
 
-		return ret + 1;
+		return ret;
 	}
 };
