d1548dd166264044bce7bb0c9bddc61470d45fad
ordinarydennis <ordinarydennis@gmail.com>
2023-10-15 23:03:17 +0900

    Edit Leet Code Solution - 845. Longest Mountain in Array(solved again, 30m)

--- Dynamic Programming/845_Longest_Mountain_in_Array.h
+++ Dynamic Programming/845_Longest_Mountain_in_Array.h
@@ -2,30 +2,33 @@ class Solution {
 public:
 	int longestMountain(vector<int>& arr) {
 
-		int max = static_cast<int>(arr.size());
-
 		int start = 0;
-		int end = 0;
+		int max = static_cast<int>(arr.size());
 
 		int ret = 0;
 
 		while (start < max)
 		{
-			end = start;
+			int end = start;
 
 			if (end + 1 < max && arr[end] < arr[end + 1])
 			{
-				while (end + 1 < max && arr[end] < arr[end + 1]) end++;
+				while (end + 1 < arr.size() && arr[end] < arr[end + 1])
+				{
+					end++;
+				}
 
 				if (end + 1 < max && arr[end] > arr[end + 1])
 				{
-					while (end + 1 < max && arr[end] > arr[end + 1]) end++;
-
+					while (end + 1 < max && arr[end] > arr[end + 1])
+					{
+						end++;
+					}
 					ret = std::max(ret, end - start + 1);
 				}
 			}
 
-			start = std::max(end, start + 1);
+			start = std::max(start + 1, end);
 		}
 
 		return ret;
