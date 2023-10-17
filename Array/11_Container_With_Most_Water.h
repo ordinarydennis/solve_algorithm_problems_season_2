999eeeef39f24b9c4ced3ade917a93279981c79c
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-10-17 13:13:43 +0900

    Edit LeetCode Solution - 11. Container With Most Water(solved again)

--- Array/11_Container_With_Most_Water.h
+++ Array/11_Container_With_Most_Water.h
@@ -1,3 +1,5 @@
+
+//first approch(time limit)
 class Solution {
 public:
 	int maxArea(vector<int>& height) {
@@ -13,6 +15,38 @@ public:
 				int h = std::min(height[a], height[b]);
 				ret = std::max(ret, h * (b - a));
 				m = h;
+			}
+		}
+
+		return ret;
+	}
+};
+
+
+//two point
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+
+		int l = 0;
+
+		int r = static_cast<int>(height.size() - 1);
+
+		int ret = 0;
+
+		while (l < r)
+		{
+			int h = std::min(height[l], height[r]);
+
+			ret = std::max(ret, h * (r - l));
+
+			if (height[l] < height[r])
+			{
+				l++;
+			}
+			else
+			{
+				r--;
 			}
 		}
 
