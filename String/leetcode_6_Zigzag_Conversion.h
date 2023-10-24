9384364bb956a01dee67e26901842d64e810ff68
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-10-24 14:02:56 +0900

    Edit LeetCode Solution - 6. Zigzag Conversion(fail) https://leetcode.com/problems/zigzag-conversion/solutions/3134053/c-faster-than-95-easy-understanding-clean-concise-code/

--- String/leetcode_6_Zigzag_Conversion.h
+++ String/leetcode_6_Zigzag_Conversion.h
@@ -1,3 +1,5 @@
+
+//first approach
 class Solution {
 public:
 	string convert(string s, int numRows) {
@@ -58,3 +60,36 @@ public:
 		return ret;
 	}
 };
+
+//second approach
+class Solution {
+public:
+	string convert(string s, int numRows) {
+
+		if (numRows <= 1) return s;
+
+		std::vector<string> slist(numRows, "");
+
+		int p = 0;
+		int dir = 1;
+
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (p == 0 || p == numRows - 1) dir *= -1;
+
+			slist[p] += s[i];
+
+			if (-1 == dir) p++;
+			else p--;
+		}
+
+		string ret;
+
+		for (auto& str : slist)
+			ret += std::move(str);
+
+		return ret;
+	}
+};
+
+//https://leetcode.com/problems/zigzag-conversion/solutions/3134053/c-faster-than-95-easy-understanding-clean-concise-code/
