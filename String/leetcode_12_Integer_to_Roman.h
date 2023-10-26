cdacadd93ed382b934446b3658672da0e8b66b12
ordinarydennis <ordinarydennis@gmail.com>
2023-10-26 22:11:39 +0900

    Edit LeetCode Solution - 12. Integer to Roman(read solution) https://leetcode.com/problems/integer-to-roman/solutions/3216797/easiest-beginner-friendly-sol-c-java-python/

--- String/leetcode_12_Integer_to_Roman.h
+++ String/leetcode_12_Integer_to_Roman.h
@@ -2,85 +2,35 @@ class Solution {
 public:
 	string intToRoman(int num) {
 
-		int r = num;
-
-		vector<string> strList;
-
-		int de = 1;
-
-		while (r)
-		{
-			int n = r % 10;
-
-			n *= de;
-
-			string str;
-
-			if (0 == n || 10 == n)
-			{
-				str += "X";
-			}
-			else if (5 == n)
-			{
-				str += "V";
-			}
-			else if (4 == n)
-			{
-				str += "IV";
-			}
-			else if (9 == n)
-			{
-				str += "IX";
-			}
-			else if (n < 4)
-			{
-				for (int i = 0; i < n; i++)
-				{
-					str += "I";
-				}
-			}
-			else if (n < 9)
-			{
-				str += "V";
-				for (int i = 0; i < n - 5; i++)
-				{
-					str += "I";
-				}
-			}
-			else if (n == 50)
-			{
-				str += "L";
-			}
-			else if (n == 90)
-			{
-				str += "XC";
-			}
-			else if (n == 900)
-			{
-				str += "CM";
-			}
-			else if (n == 1000)
-			{
-				str += "M";
-			}
-
-			strList.push_back(str);
-
-			r = r / 10;
-
-			de *= 10;
-		}
-
-
-		int len = strList.size();
+		vector<pair<int, string>>  clist = {
+			{1000, "M"},
+			{900, "CM"},
+			{500, "D"},
+			{400, "CD"},
+			{100, "C"},
+			{90, "XC"},
+			{50, "L"},
+			{40, "XL"},
+			{10, "X"},
+			{9, "IX"},
+			{5, "V"},
+			{4, "IV"},
+			{1, "I"}
+		};
 
 		string ret;
 
-		for (int i = len - 1; i >= 0; i--)
+		for (int i = 0; i < clist.size(); i++)
 		{
-			ret += strList[i];
+			while (num >= clist[i].first)
+			{
+				ret += clist[i].second;
+				num -= clist[i].first;
+			}
 		}
 
 		return ret;
 	}
 };
+
+//https://leetcode.com/problems/integer-to-roman/solutions/3216797/easiest-beginner-friendly-sol-c-java-python/
