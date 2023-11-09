960f1961c52f8605798f574a431b133ea0841e9f
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-11-09 13:49:19 +0900

    Edit LeetCode Solution - 33. Search in Rotated Sorted Array(read solution) https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/3879263/100-binary-search-easy-video-o-log-n-optimal-solution/

--- Array/33_Search_in_Rotated_Sorted_Array.h
+++ Array/33_Search_in_Rotated_Sorted_Array.h
@@ -1,3 +1,44 @@
+
+//second approach
+class Solution {
+
+public:
+	int search(vector<int>& nums, int target) {
+
+		int l = 0;
+		int r = nums.size() - 1;
+
+		while (l <= r)
+		{
+			int mid = (l + r) / 2;
+
+			if (nums[mid] == target)
+				return mid;
+
+			if (nums[l] <= nums[mid])
+			{
+				if (nums[l] <= target && target <= nums[mid])
+					r = mid;
+				else
+					l = mid + 1;
+			}
+			else
+			{
+				if (nums[mid] < target && target <= nums[r])
+					l = mid + 1;
+				else
+					r = mid;
+			}
+		}
+
+		return -1;
+	}
+};
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/3879263/100-binary-search-easy-video-o-log-n-optimal-solution/
+
+
+//first approach
 class Solution {
 
 	void search(vector<int>& nums, int start, int end, int target, int& ret)
