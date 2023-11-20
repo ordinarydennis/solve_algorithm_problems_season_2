2ae0afdebde685372b1b7b74963b4cbf4396c718
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-11-20 14:11:44 +0900

    Edit LeetCode Solution - 36. Valid Sudoku(23m, solved again)

--- Array/36_Valid_Sudoku.h
+++ Array/36_Valid_Sudoku.h
@@ -42,3 +42,32 @@ public:
 //leetcode.com/problems/valid-sudoku/solutions/15464/my-short-solution-by-c-o-n2/?orderBy=most_votes
 
 
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+
+		int rMat[9][9] = {};
+		int cMat[9][9] = {};
+		int mMat[9][9] = {};
+
+		for (int a = 0; a < 9; a++)
+		{
+			for (int b = 0; b < 9; b++)
+			{
+				if ('.' == board[a][b]) 
+					continue;
+
+				int val = board[a][b] - '0' - 1;
+
+				int i = ((a / 3) * 3) + (b / 3);
+
+				if (rMat[a][val] || cMat[b][val] || mMat[i][val])
+					return false;
+
+				rMat[a][val] = cMat[b][val] = mMat[i][val] = 1;
+			}
+		}
+
+		return true;
+	}
+};
