63fcac2e8c33efb7806a1c3421c6bfd73bc31508
JUNYOUNGOH-1\junyoungoh <ordinarydennis@gmail.com>
2023-12-14 14:01:03 +0900

    Edit LeetCode Solution - 851. Loud and Rich(read solution) https://leetcode.com/problems/loud-and-rich/description/

--- Graph/851_Loud_and_Rich.h
+++ Graph/851_Loud_and_Rich.h
@@ -3,15 +3,17 @@ class Solution {
 
 	int dfs(vector<vector<int>>& mat, int person, vector<int>& quiet)
 	{
-		int min = INT_MAX;
+		int min = quiet[person];
 
 		for (int i = 0; i < mat[person].size(); i++)
 		{
 
 			min = std::min(
-				quiet[i],
+				quiet[person],
 				dfs(mat, mat[person][i], quiet)
 			);
+
+			mat[person] = min;
 		}
 
 		return min;
@@ -22,21 +24,23 @@ public:
 	vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
 
 
-		vector<vector<int>> mat(quiet.size() + 1);
+		vector<vector<int>> mat(richer.size());
 
-		for (int i = 0;i < quiet.size(); i++)
+		for (int i = 0;i < richer.size(); i++)
 		{
-			mat[richer[1]].push_back(richer[0]);
+			mat[richer[i][1]].push_back(richer[i][0]);
 		}
 
 		vector<int> ret;
 
 		for (int i = 0; i < quiet.size(); i++)
 		{
-			int m = dfs(vector<vector<int>>&mat, int person, vector<int>&quiet);
+			int m = dfs(mat, i, quiet);
 			ret.push_back(m);
 		}
 
 		return ret;
 	}
 };
+
+//leetcode.com/problems/loud-and-rich/editorial/
