f74b7f65630fad3fc9a747ffee7d7d1b8f12a075
ordinarydennis <ordinarydennis@gmail.com>
2023-12-26 23:16:38 +0900

    Edit LeetCode Solution - 394. Decode String(retry, fail)

--- String/leetcode_394_Decode_String.h
+++ String/leetcode_394_Decode_String.h
@@ -39,3 +39,74 @@ public:
 
 	return ret;
 };
+
+class Solution {
+public:
+	string decodeString(string s) {
+
+		stack<int> ns;
+		stack<string> nstr;
+
+		string base;
+
+		string ret;
+
+		string str;
+
+		for (int i = 0; i < s.size(); i++k)
+		{
+			char c = s[i];
+
+			if (isdigit(c))
+			{
+				ns.push(c - '0');
+
+				if (base.size())
+				{
+					nstr.push(std::move(base));
+				}
+			}
+			else if ('[' == c)
+			{
+				openCount++;
+				
+				if (base.size())
+				{
+					nstr.push(std::move(base));
+				}
+			}
+			else if (']' == c)
+			{
+				openCount--;
+
+				nstr.push(std::move(base));
+
+				int r = ns.top();
+				ns.pop();
+				int str = ns.top();
+				ns.pop();
+
+				for (int a = 0; a < r - 1; a++)
+				{
+					str += str;
+				}
+
+				base.insert(0, str);
+
+				if (openCount == 0)
+				{
+					ret += base;
+					base.clear();
+				}
+			}
+			else
+			{
+				base += c;
+			}
+		}
+
+
+		return ret;
+
+	}
+};
